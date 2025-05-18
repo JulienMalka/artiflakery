35e29a5b5204a105d4a04e6d0d0225327fde5eef
Julien Malka <julien@malka.sh>
2025-05-18 19:17:39 +0200

    feat: intro nix packaging

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Artiflakery";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        artiflakery = pkgs.haskellPackages.callPackage ./package.nix { };
+      in
+      rec {
+        defaultPackage = artiflakery;
+        checks = {
+          inherit artiflakery;
+        };
+      }
+    ));
+}
