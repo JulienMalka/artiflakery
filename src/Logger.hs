e41c030e21deda6e58084fec38ad4d2fdad85092
Julien Malka <julien@malka.sh>
2025-05-20 21:11:27 +0200

    chore: silence some warnings

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,52 +1,48 @@
-module Logger
-  (coloredLogAction)
-  where
+module Logger (coloredLogAction) where
 
 import Colog
-import Colog.Actions (logTextStdout)
-import Data.Time.Clock (UTCTime)
-import Data.Time (getCurrentTime, formatTime, defaultTimeLocale)
-import System.Console.ANSI (Color(..), ColorIntensity(..), ConsoleLayer(..), SGR(..), setSGRCode)
-import Data.Text (Text)
 import qualified Data.Text as T
-
+import Data.Time (defaultTimeLocale, formatTime, getCurrentTime)
+import Data.Time.Clock (UTCTime)
+import System.Console.ANSI (Color (..), ColorIntensity (..), ConsoleLayer (..), SGR (..), setSGRCode)
 
 padRight :: Int -> Char -> T.Text -> T.Text
-padRight width char text = 
-    let textLen = T.length text
-    in if textLen >= width
-       then text
-       else text <> T.replicate (width - textLen) (T.singleton char)
-
+padRight width char text =
+  let textLen = T.length text
+   in if textLen >= width
+        then text
+        else text <> T.replicate (width - textLen) (T.singleton char)
 
 colorBySeverity :: Severity -> T.Text -> T.Text
 colorBySeverity sev txt = case sev of
-    Debug   -> colorize Green txt
-    Info    -> colorize Blue txt
-    Warning -> colorize Yellow txt
-    Error   -> colorize Red txt
+  Debug -> colorize Green txt
+  Info -> colorize Blue txt
+  Warning -> colorize Yellow txt
+  Error -> colorize Red txt
   where
     colorize :: Color -> T.Text -> T.Text
-    colorize c txt =
-        T.pack (setSGRCode [SetColor Foreground Vivid c])
-        <> txt
+    colorize c txt' =
+      T.pack (setSGRCode [SetColor Foreground Vivid c])
+        <> txt'
         <> T.pack (setSGRCode [Reset])
 
-
 customMessageFormat :: Severity -> UTCTime -> T.Text -> T.Text
-customMessageFormat sev time msg = 
-    let formattedText = T.intercalate " | " 
-            [ padRight 8 ' ' (T.pack $ show sev)
-            , T.pack $ formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" time
-            , msg
-            ]
-    in colorBySeverity sev formattedText
-
+customMessageFormat sev time msg =
+  let formattedText =
+        T.intercalate
+          " | "
+          [ padRight 8 ' ' (T.pack $ show sev),
+            T.pack $ formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" time,
+            msg
+          ]
+   in colorBySeverity sev formattedText
 
 coloredLogAction :: LogAction IO Message
-coloredLogAction = 
-    filterBySeverity Debug msgSeverity $
-    cmapM (\msg -> do
-        time <- getCurrentTime
-        pure $ customMessageFormat (msgSeverity msg) time (msgText msg)
-    ) logTextStdout
+coloredLogAction =
+  filterBySeverity Debug msgSeverity $
+    cmapM
+      ( \msg -> do
+          time <- getCurrentTime
+          pure $ customMessageFormat (msgSeverity msg) time (msgText msg)
+      )
+      logTextStdout
